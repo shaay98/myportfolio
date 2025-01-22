db25b0f6813f766ffea9083ea81ec72a29f9148c
shaay98 <nrichardson567@icloud.com>
2025-01-22 02:35:37 +0000

    attempt

---
+++ contact.js
@@ -0,0 +1,6 @@
+"use strict"
+
+const btnTwo = document.getElementById("done-btn");
+btnTwo.addEventListener("click", function () {
+  alert("Thanks for your response, I will contact you when I get a chance");
+});
--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-"use strict"
+"use strict";
 
+const buttonOne = document.getElementById("my-btn");
+buttonOne.addEventListener("click", function () {
+  alert("Thanks for your response, I will contact you when I get a chance");
+});
+
+const btnTwo = document.getElementById("done-btn");
+btnTwo.addEventListener("click", function () {
+  alert("Thanks for your response, I will contact you when I get a chance");
+});
