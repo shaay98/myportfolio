4eafba9cf1aa1e121053a8d3485cfc8d36ff3cad
shaay98 <nrichardson567@icloud.com>
2025-01-23 01:18:56 +0000

    attempt

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const contactList = [
+  "Email richardson123@yahoo.com",
+  "Phone 1-(888)-937-6259",
+  "Address 123 Apple St.",
+  "City Atlanta",
+  "State Georgia",
+];
+
+const ul = document.getElementById("contactList");
+ul.innerHTML = "";
+
+for (let i = 0; i < contactList.length; i++) {
+  const ul = document.getElementById("contactList");
+  ul.innerHTML += "<li>New List Item</li>";
+  li.textContent = contactList[i];
+  ul.innerHTML(li);
+}
+
 const buttonOne = document.getElementById("my-btn");
 buttonOne.addEventListener("click", function () {
   alert("Thanks for visiting, let me know what you think.");
